986eefab62cc06af595cba35c1e095441eb6d673
Chromodyne <dsinitiere@outlook.com>
2022-09-04 12:19:58 -0700

    Teletype mode testing. Prints Hello

--- bootloader.asm
+++ bootloader.asm
@@ -1,5 +1,16 @@
-loop:
-	jmp	loop
+	mov	ah, 0x0E	;Enter teletype (TTY) mode
+	mov	al, 'H'		;Store H char in lower AX register.
+	int	0x10		;General video interrupt.
+	mov	al, 'E'
+	int	0x10
+	mov	al, 'L'
+	int	0x10
+	mov	al, 'L'
+	int 	0x10
+	mov	al, 'O'
+	int	0x10
+
+jmp $
 
 times 510-($-$$) db 0 	;Write zeroes.
 dw	0xAA55		;Flag sector as bootable.
